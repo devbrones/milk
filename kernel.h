9f3bd584498e1cf744e1a7c8661b3c4e982c6691
devbrones <34194695+devbrones@users.noreply.github.com>
2021-01-29 00:09:09 +0100

    Create kernel.h
    first commit

---
+++ kernel.h
@@ -0,0 +1,18 @@
+#define VGA_ADDRESS 0xB8000
+#define BLACK 0
+#define BLUE 1
+#define GREEN 2
+#define CYAN 3
+#define RED 4
+#define PURPLE 5
+#define BROWN 6
+#define LGRAY 7
+#define DGRAY 8
+#define LBLUE 9
+#define LGREEN 10
+#define LCYAN 11
+#define LRED 12
+#define PINK 13 
+#define YELLOW 14 
+#define WHITE_COLOR 15
+
