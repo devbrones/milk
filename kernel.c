5f805d9a57cb8ffb7d8af6ffd21e799b3b973013
devbrones <34194695+devbrones@users.noreply.github.com>
2021-01-29 10:42:00 +0100

    Update kernel.c
    added finish prompt

--- kernel.c
+++ kernel.c
@@ -11,6 +11,18 @@ void main(void)
 	print_string("ashes", YELLOW);
 	newline();    /* calls the nextline function in vgaout.h */
 	print_string("to ashes", RED);
+	newline();
+	print_string("╔═════════════════════════════╗", PINK);
+	newline();
+	print_string("║		Kernel has finished.	║", PINK);
+	newline();
+	print_string("║	   Reboot the computer to	║", PINK);
+	newline();
+	print_string("║		  reinitialize the		║", PINK);
+	newline();
+	print_string("║				kernel			║", PINK);
+	newline();
+	print_string("╚═════════════════════════════╝", PINK);
 	return;
 }
 
