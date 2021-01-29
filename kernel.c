96087c38be50720589d12e8bbe334cbb420b208b
devbrones <34194695+devbrones@users.noreply.github.com>
2021-01-29 08:55:01 +0100

    added support for kernel.h

--- kernel.c
+++ kernel.c
@@ -1,11 +1,7 @@
 #define VGA_ADDRESS 0xB8000   /* video memory begins here. */
 
 /* VGA provides support for 16 colors */
-#define BLACK 0
-#define GREEN 2
-#define RED 4
-#define YELLOW 14
-#define WHITE_COLOR 15
+#include <kernel.h>
 
 unsigned short *terminal_buffer;
 unsigned int vga_index;
