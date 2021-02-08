b890ac5f011231b932410b9ad6b0a0028918bbb6
devbrones <34194695+devbrones@users.noreply.github.com>
2021-02-08 11:56:37 +0100

    Update kbh.h

--- alpha/kbh.h
+++ alpha/kbh.h
@@ -9,12 +9,12 @@ void keyboard_handler_main(void) {
 	char keycode;
 
 	/* write EOI */
-	write_port(0x20, 0x20);
+	kbsend(0x20, 0x20);
 
-	status = read_port(KEYBOARD_STATUS_PORT);
+	status = kbin(KEYBOARD_STATUS_PORT);
 	/* Lowest bit of status will be set if buffer is not empty */
 	if (status & 0x01) {
-		keycode = read_port(KEYBOARD_DATA_PORT);
+		keycode = kbin(KEYBOARD_DATA_PORT);
 		if(keycode < 0)
 			return;
 		vidptr[current_loc++] = keyboard_map[keycode];
